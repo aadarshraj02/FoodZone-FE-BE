9fa24c6fd75a495a8340514d10ebe44434b09af6
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-20 13:57:26 +0530

    added use effect

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const BASE_URL = "http://localhost:9000/";
@@ -10,20 +10,21 @@ const App = () => {
 
   const [error, setError] = useState(null);
 
-  const fetchFoodData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(BASE_URL);
-      const json = await response.json();
+  useEffect(() => {
+    const fetchFoodData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(BASE_URL);
+        const json = await response.json();
 
-      setData(json);
-      setLoading(false);
-    } catch (error) {
-      setError("Unable to fetch data");
-    }
-  };
-
-  fetchFoodData();
+        setData(json);
+        setLoading(false);
+      } catch (error) {
+        setError("Unable to fetch data");
+      }
+    };
+    fetchFoodData();
+  });
 
   if (error) {
     return <div>{error}</div>;
