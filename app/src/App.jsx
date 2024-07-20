b10769941d1917e5839496c1c5999c8e20742be1
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-20 09:55:03 +0530

    added a main conatiner

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,9 @@
+import styled from "styled-components";
+
 const App = () => {
-  return <div>App</div>;
+  return <MainContainer></MainContainer>;
 };
 
 export default App;
+
+const MainContainer = styled.div``;
